d102ad7b3ab77ee6576916eb3b4ca63aa5b0d4e9
Gopal <Gopal1409@gmail.com>
2021-07-16 22:18:06 -0700

    terraform module

--- location/location.tf
+++ location/location.tf
@@ -2,7 +2,7 @@ provider "azurerm" {
     features {}
 }
 locals {
-    web_server_name = var.environment == "production" ? "${var.}-prd" : "{var.web_server_name}-dev"
+    web_server_name = var.environment == "production" ? "${var.web_server_name}-prd" : "{var.web_server_name}-dev"
     build_environment = var.environment == "production" ? "production":"development"
 }
 resource "azurerm_resource_group" "web_server_rg" {
